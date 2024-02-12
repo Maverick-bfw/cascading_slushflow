5756c033eae1090d567d5942aac988aa1135c1dc
snowman <Christopher.damboise@uit.no>
2024-02-12 17:06:15 +0100

    The two methods are combined and now the edge of the lake is outlined. Next step is to find the min values of the ring and save them as release layers.

--- lake_outline.py
+++ lake_outline.py
@@ -1,13 +1,9 @@
 import rasterio
 import numpy as np
-from skimage.morphology import binary_dilation
-from skimage.measure import label
-from skimage.morphology import erosion, label
+from skimage.morphology import binary_dilation, erosion, label
 
 lake_file = "/media/snowman/LaCie/cascading_slushflow/fonnbu/output_lakes_raster.tif"
 output_file = "/media/snowman/LaCie/cascading_slushflow/fonnbu/rings.tif"
-
-
 
 with rasterio.open(lake_file) as lake_src:
     lake = lake_src.read(1)  # Read lake raster data
@@ -20,7 +16,7 @@ with rasterio.open(lake_file) as lake_src:
     binary_lake = lake > threshold
 
     # Label connected regions (clusters) in the binary lake raster
-    labeled_lake = label(binary_lake)
+    labeled_lake = label(binary_lake, connectivity=2)  # Set connectivity to 2 for diagonal connections
 
     # Initialize the output data array with zeros
     output_data = np.zeros_like(lake)
@@ -29,11 +25,21 @@ with rasterio.open(lake_file) as lake_src:
     for cluster_label in np.unique(labeled_lake):
         if cluster_label == 0:  # Skip background label (0)
             continue
+        if cluster_label > 60:
+            continue
 
+        print(cluster_label)
         # Create a binary mask for the current cluster
         cluster_mask = labeled_lake == cluster_label
+
         # Erode the cluster mask to include diagonal cells
-        #eroded_mask = erosion(cluster_mask, np.ones((3, 3)))
+        eroded_mask = erosion(cluster_mask, np.ones((3, 3)))
+
+        # Find the difference between the eroded mask and the original mask
+        outline = cluster_mask ^ eroded_mask
+
+        # Assign the cluster label to the cells in the outline
+        output_data[outline] = cluster_label
 
         # Perform dilation to get the surrounding ring
         ring = binary_dilation(cluster_mask) & ~cluster_mask
